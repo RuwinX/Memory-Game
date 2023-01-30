0c34051930d0bb539cd4db84b47dab8205de4b9f
RuwinX <amit.rj1337@gmail.com>
2023-01-31 01:10:59 +0530

    Adding missing asset file for one of the alphabet. Added a list of the mapping of letters to assets in a separate .txt file Modified the function responsible for randomization of the deck to be more random and efficient with no repetitions Prototype code now randomizes and reveals the tiles.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
+import random
+import time
+
 import pygame
 import ctypes
 
 pygame.init()
 
-#Disable windows display scaling from affecting the pygame display.
+# Disable windows display scaling from affecting the pygame display.
 ctypes.windll.user32.SetProcessDPIAware()
 
 # set the screen size
@@ -17,35 +20,138 @@ yPos = 170
 logo = pygame.image.load("../src/assets/logo/memory-loss.png")
 pygame.display.set_icon(logo)
 
-cardSpriteLocations={}
+cardSpriteLocations = {}
 rectSpriteList = []
-def cardReveal(xCoordinate,yCoordinate):
-    a = pygame.image.load("A.png")
-    screen.blit(a, (xCoordinate, yCoordinate))
 
-def hiddenCardRenderer(xCoordinate,yCoordinate):
+pairList = []
+letterMapDict = {}
+value_matrixDict = {}
+value_matrix = [[None for i in range(6)] for j in range(6)]
+revealedTileCount = 0
+def letterMapper():
+    with open("../src/assets/imageMapper.txt") as f:
+        for line in f:
+            (key, val) = line.split()
+            letterMapDict[str(key)] = val
+def pairListGenerator():
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    listOfAlphabet = []
+    for i in range(len(alphabet)):
+        listOfAlphabet.append(alphabet[i])
+    random.shuffle(listOfAlphabet)
+    random.shuffle(listOfAlphabet)
+    for i in range(18):
+        index = random.randrange(len(listOfAlphabet))
+        pairList.append(random.choice(listOfAlphabet[index]))
+        del listOfAlphabet[index]
+    pairList.extend(pairList)
+    random.shuffle(pairList)
+
+
+
+def cardHide(rectCoordinates,image):
+    for i in range(255):
+        pygame.Surface.fill(screen, (0, 0, 0), rez)
+        opacity = 255 - i
+        # time.sleep(0.1)
+        card2 = pygame.image.load("../src/assets/cards/letter-a.png")
+        card2.set_alpha(opacity)
+        screen.blit(card2, (rez.left, rez.top))
+        pygame.display.flip()
+
+
+def populateMatrix():
+    index = 0
+    for i in range(6):
+        for j in range(6):
+                value_matrix[i][j] = pairList[index]
+                value_matrixDict[(i,j)] = pairList[index]
+                index += 1
+
+
+def cardReveal(rectCoordinates):
+    indexTobeRevealed = list(cardSpriteLocations.keys())[list(cardSpriteLocations.values()).index(rectCoordinates)]
+    #the coordinates
+    #print(indexTobeRevealed) -- (0,0)
+    #the letter
+    #value_matrixDict[indexTobeRevealed] -- m
+    # the mapping to asset
+    #letterMapDict[value_matrixDict[indexTobeRevealed]]
+    for i in range(255):
+        pygame.Surface.fill(screen, (0, 0, 0), rez)
+        opacity = i
+        # time.sleep(0.1)
+        card2 = pygame.image.load("../src/assets/cards/"+letterMapDict[value_matrixDict[indexTobeRevealed]])
+        card2.set_alpha(opacity)
+        screen.blit(card2, (rez.left, rez.top))
+        pygame.display.flip()
+
+
+def tileSelector(rectCoordinates):
+    indexTobeRevealed = list(cardSpriteLocations.keys())[list(cardSpriteLocations.values()).index(rectCoordinates)]
+    # the coordinates
+    # print(indexTobeRevealed) -- (0,0)
+    # the letter
+    # value_matrixDict[indexTobeRevealed] -- m
+    # the mapping to asset
+    # letterMapDict[value_matrixDict[indexTobeRevealed]]
+
+    # training_data_x = [x[0] for x in indexTobeRevealed]
+    # print(training_data_x)
+    xCoordinate = indexTobeRevealed[0]
+    yCoordinate = indexTobeRevealed[1]
+    if revealedTileCount == 0:
+        revealedTileCount += 1
+        revealedTileValue = value_matrix[xCoordinate][yCoordinate]
+        display_matrix[xCoordinate][yCoordinate] = self.value_matrix[xCoordinate][yCoordinate]
+        revealedTileXCoordinate = xCoordinate
+        revealedTileYCoordinate = yCoordinate
+    else:
+        if (revealedTileValue == self.value_matrix[xCoordinate][yCoordinate]):
+            display_matrix[xCoordinate][yCoordinate] = self.value_matrix[xCoordinate][yCoordinate]
+            revealedTileCount = 0
+            revealedPaircount += 1
+        else:
+            self.display_matrix[xCoordinate][yCoordinate] = self.value_matrix[xCoordinate][yCoordinate]
+            self.print_matrix()
+
+            self.display_matrix[self.revealedTileXCoordinate][self.revealedTileYCoordinate] = "?"
+            self.display_matrix[xCoordinate][yCoordinate] = "?"
+            self.revealedTileCount = 0
+            self.revealedTileYCoordinate = None
+            self.revealedTileXCoordinate = None
+            self.revealedTileValue = None
+
+
+
+def hiddenCardRenderer(xCoordinate, yCoordinate):
     y = yCoordinate
-    for i in range(0,6):
+    for i in range(0, 6):
         x = xCoordinate
-        for j in range(0,6):
-            #pygame.Surface.fill(screen, (160, 160, 160), pygame.Rect((x, y), (128, 128)))
+        for j in range(0, 6):
             card = pygame.image.load("../src/assets/misc/hiddenCard.png")
-            screen.blit(card, (x,y ))
-            cardSpriteLocations[(i,j)] = pygame.Rect((x, y), (128, 128))
+            screen.blit(card, (x, y))
+            cardSpriteLocations[(i, j)] = pygame.Rect((x, y), (128, 128))
             rectSpriteList.append(pygame.Rect((x, y), (128, 128)))
             # 128 is the card dimension + 20 more for padding
             x = x + 148
         y = y + 148
 
 
-hiddenCardRenderer(xPos,yPos)
+hiddenCardRenderer(xPos, yPos)
 running = True
+#print(rectSpriteList)
 print(cardSpriteLocations)
+pairListGenerator()
+populateMatrix()
+letterMapper()
+print(letterMapDict)
+print(value_matrix)
+print(value_matrixDict)
 
 while running:
     # change the screen color to a rgb value
-    #screen.fill((160, 160, 160))
-
+    # screen.fill((160, 160, 160))
     for event in pygame.event.get():
         # making sure the game closes when the X button is hit
         if event.type == pygame.QUIT:
@@ -54,11 +160,10 @@ while running:
             mousePointerPosition = pygame.mouse.get_pos()
             print(mousePointerPosition)
             for rez in rectSpriteList:
-                if pygame.Rect.collidepoint(rez,mousePointerPosition):
-                    print("VALID!")
-                    pygame.Surface.fill(screen, (160,160,160),rez)
-                    print(rez)
-            #cardReveal(20,170)
-    # update the display with the new screen color
+                if pygame.Rect.collidepoint(rez, mousePointerPosition):
+                    cardReveal(rez)
+                    #tileSelector(rez)
+                    #cardHide(rez)
 
+    # update the display with the new screen color
     pygame.display.update()
