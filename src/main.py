7ea7a2a8ad1a08d8a2251a0748b22f81f948cc5c
RuwinX <amit.rj1337@gmail.com>
2023-02-13 20:13:41 +0530

    Added comments for the various functions defined for reference later

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from pygame import mixer
 
 pygame.init()
 
-# Disable windows display scaling from affecting the pygame display.
+# Disable windows display scaling from affecting the pygame display. Remove this line if you want display scaling to affect.
 ctypes.windll.user32.SetProcessDPIAware()
 
 pygame.mixer.pre_init(44100, 16, 2, 4096)
@@ -39,15 +39,15 @@ revealedTileValue = ""
 revealedPaircount = 0
 revealedTileRectValue = pygame.Rect
 
-
+#This function is used to remove a specific rect from the list to remove any further collision events for it from being detected.
 def rectRemover(rectCoordinates):
     rectSpriteList.remove(rectCoordinates)
 
-
+#This function is to re-add a specific rect which was removed from the rectList of sprites in case an uneven pair was discovered.
 def rectAdder(rectCoordinates):
     rectSpriteList.append(rectCoordinates)
 
-
+#This function maps the asset value between the letters and their corresponding assets from the text file
 def letterMapper():
     with open("../src/assets/imageMapper.txt") as f:
         for line in f:
@@ -55,6 +55,7 @@ def letterMapper():
             letterMapDict[str(key)] = val
 
 
+#This function is used to randomize 18 letters from the 26 letter alphabet to form pairs  with no repetitions.
 def pairListGenerator():
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     listOfAlphabet = []
@@ -69,7 +70,7 @@ def pairListGenerator():
     pairList.extend(pairList)
     random.shuffle(pairList)
 
-
+#This function is used to hide the image displayed at a specific rect and replace it with the image provided.
 def cardHide(rectCoordinates, image):
     for i in range(255):
         pygame.Surface.fill(screen, (0, 0, 0), rectCoordinates)
@@ -80,7 +81,7 @@ def cardHide(rectCoordinates, image):
         screen.blit(card, (rectCoordinates.left, rectCoordinates.top))
         pygame.display.flip()
 
-
+#This function is used to populate a 6x6 matrix with the random generated pairings of the 18 different alphabet pairs from  =pairListGenerator()
 def populateMatrix():
     index = 0
     for i in range(6):
@@ -89,7 +90,7 @@ def populateMatrix():
             value_matrixDict[(i, j)] = pairList[index]
             index += 1
 
-
+#This function is used to animate and reveal the asset linked behind the card RECT to the corresponding letter from the valueMatrix. Eg- the Rect contains the letter A. The asset image for A is revealed.
 def cardReveal(rectCoordinates):
     indexTobeRevealed = list(cardSpriteLocations.keys())[list(cardSpriteLocations.values()).index(rectCoordinates)]
     for i in range(255):
@@ -101,21 +102,13 @@ def cardReveal(rectCoordinates):
         screen.blit(card, (rectCoordinates.left, rectCoordinates.top))
         pygame.display.flip()
 
-
+#This function is used to reveal the identify the tile to be revealed based on the Rect Coordinates. Depending on the situation, either the card is revealed[if its the first card[, revealed and then hidden, in case is the second card to be revealed and both cards are not a match, the card is the second card to be revealed and they are both a match.
 def tileSelector(rectCoordinates):
     global revealedTileCount
     global revealedTileValue
     global revealedPaircount
     global revealedTileRectValue
     indexTobeRevealed = list(cardSpriteLocations.keys())[list(cardSpriteLocations.values()).index(rectCoordinates)]
-    # the coordinates
-    # print(indexTobeRevealed) -- (0,0)
-    # the letter
-    # value_matrixDict[indexTobeRevealed] -- m
-    # the mapping to asset
-    # letterMapDict[value_matrixDict[indexTobeRevealed]]
-    # training_data_x = [x[0] for x in indexTobeRevealed]
-    # print(training_data_x)
     xCoordinate = indexTobeRevealed[0]
     yCoordinate = indexTobeRevealed[1]
     if revealedTileCount == 0:
@@ -153,7 +146,7 @@ def tileSelector(rectCoordinates):
         pygame.time.delay(1000)
         gameOver()
 
-
+#This function handles the toggling on/off of the background music when the volume icon is selected.
 def volumeButton(xCoordinate, yCoordinate):
     muteButtonRect = pygame.Rect((20, 30), (128, 128))
     for i in range(255):
@@ -164,7 +157,7 @@ def volumeButton(xCoordinate, yCoordinate):
         screen.blit(card, (xCoordinate, yCoordinate))
         pygame.display.flip()
 
-
+#this function is used to render the initial 6x6 grid with the card logo at the start of the game
 def hiddenCardRenderer(xCoordinate, yCoordinate):
     y = yCoordinate
     screen.set_alpha(0)
@@ -188,7 +181,7 @@ def hiddenCardRenderer(xCoordinate, yCoordinate):
         screen.blit(board_surface, (0, 0))
         pygame.display.flip()
 
-
+#This function is used to display the splash screen logo at the start of the game.
 def splashScreenShow():
     for i in range(255):
         # pygame.Surface.fill(screen, (0, 0, 0), ())
@@ -209,7 +202,7 @@ def splashScreenShow():
         screen.blit(image, (0, 0))
         pygame.display.flip()
 
-
+#This function is called to display the game over screen
 def gameOver():
     gameOver_surface = pygame.Surface.copy(screen)
     for i in range(255):
@@ -232,24 +225,23 @@ def gameOver():
         pygame.display.flip()
         running = False
 
-
+#this function is used to play the sound effect when a card is selected to be revealed.
 def selectSound():
     selectSound = mixer.Sound("../src/assets/sounds/select.mp3")
     selectSound.play()
 
-
+#this function is used to play the sound effect when the second card selected to be revealed, is a match to the first card revealed.
 def correctSound():
     correctSelectSound = mixer.Sound("../src/assets/sounds/correct.ogg")
     correctSelectSound.play()
 
-
+#this function is used to play the sound effect when the second card selected to be revealed, is not a match to the first card revealed.
 def wrongSound():
     wrongSelectSound = mixer.Sound("../src/assets/sounds/wrong.ogg")
     wrongSelectSound.play()
 
-
+#basic gameplay loop
 splashScreenShow()
-
 hiddenCardRenderer(xPos, yPos)
 volumeButton(20, 30)
 running = True
@@ -258,6 +250,7 @@ populateMatrix()
 letterMapper()
 #print(value_matrix)
 muteButtonRect = pygame.Rect((20, 30), (128, 128))
+#main game logic loop
 while running:
     for event in pygame.event.get():
         # making sure the game closes when the X button is hit
